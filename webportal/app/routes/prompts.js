623c513312ff76a3f92e8395909977f477c7d2b4
karmabum <karmabummer@gmail.com>
2025-04-23 20:48:17 -0700

    Update prompts.js

--- webportal/app/routes/prompts.js
+++ webportal/app/routes/prompts.js
@@ -3,21 +3,10 @@ import { inject as service } from '@ember/service';
 import DefaultRoute from 'ares-webportal/mixins/default-route';
 
 export default Route.extend(DefaultRoute, {
-  gameApi: service(),  
-  headData: service(),  
+  gameApi: service(),
+  headData: service(),
 
   model() {
-    let api = this.gameApi;
-    return api.requestOne('prompts');  
-  },
-
-  actions: {
-
-    refreshPrompts() {
-      let api = this.gameApi;
-      api.requestOne('prompts').then((newPrompts) => {
-        this.controller.set('model.prompts', newPrompts.prompts); 
-      });
-    }
+    return this.gameApi.requestOne('prompts');
   }
 });
